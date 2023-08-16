a8ad908461fa45816d168d32348646c0dcefc26d
GabrielEduardoSilva <gaebds@gmail.com>
2023-08-16 15:39:01 -0300

    Commit Big Bang

---
+++ iCalories/iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Gabriel Eduardo on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
