af1d854d7b7c1c094c7576a6d085379239566804
GabrielEduardoSilva <gaebds@gmail.com>
2023-08-17 11:03:05 -0300

    TimeFormatting Criado

---
+++ iCalories/iCalories/Helpers/TimeFormatting.swift
@@ -0,0 +1,22 @@
+//
+//  TimeFormatting.swift
+//  iCalories
+//
+//  Created by Gabriel Eduardo on 17/08/23.
+//
+
+import Foundation
+
+func calcTimeSince(date: Date) -> String {
+    let minutes = Int(-date.timeIntervalSinceNow) / 60
+    let hours = minutes / 60
+    let days = hours / 24
+    
+    if minutes < 120 {
+        return "\(minutes) minutos atrás"
+    } else if minutes >= 120 && hours < 48 {
+        return "\(hours) horas atrás"
+    } else {
+        return "\(days) dias atrás"
+    }
+}
